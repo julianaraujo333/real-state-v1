de7dcd0b178cc336f28f3b82fb426716eb006cfb
Juliana Araújo <julianaraujo555@outlook.com>
2021-01-28 15:03:00 -0300

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Nav, Logo} from './style'
+
+const Navbar = () =>{
+    return(
+        <>
+        <Nav>
+            <Logo>Alma</Logo>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Navbar/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const Nav = styled.nav`
+
+`
+
+const Logo = styled(Link)`
+    color: #fff;
+`
+
+const MenuBars = styled.i`
+
+`
+
+const navMenu = styled.div`
+
+`
