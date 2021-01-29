1bec284b007546107babab48e71ec273f6732353
Juliana Araújo <julianaraujo555@outlook.com>
2021-01-29 00:37:34 -0300

    create navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import {menuData} from './../../data/MenuData'
+import {Button} from './../Button/index.js';
 
-import {Nav, Logo} from './style'
+import {Nav, Logo, MenuBars, NavMenu, NavMenuLinks, NavBtn} from './style'
 
 const Navbar = () =>{
     return(
         <>
         <Nav>
             <Logo>Alma</Logo>
+            <MenuBars/>
+            <NavMenu>
+                {menuData.map((item, index) =>(
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+            <NavBtn>
+                <Button to="/contact" primary="true">
+                    Contact us
+                </Button>
+            </NavBtn>
         </Nav>
         </>
     )
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,18 +1,67 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components/macro'
 import {Link} from 'react-router-dom'
+import Bars from './../../images/bars.svg'
 
-const Nav = styled.nav`
-
+export const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    background: purple;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    position: fixed;
+    width: 100%;
 `
-
-const Logo = styled(Link)`
+const NavLink = css`
     color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`
+export const Logo = styled(Link)`
+    ${NavLink}
+    text-transform: uppercase;
+    font-style: italic;
+`
+export const MenuBars = styled.i`
+    display: none;
+
+    @media screen and (max-width: 768px){
+        display: block;
+        background-image: url(${Bars});
+        background-size: contain;
+        height: 40px;
+        width: 40px;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-50%, 25%);
+    }
 `
 
-const MenuBars = styled.i`
+export const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
 
+    @media screen and (max-width: 768px){
+        display: none;
+    }
 `
 
-const navMenu = styled.div`
+export const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -48px;
 
+    @media screen and (max-width: 768px){
+        display: none;
+    }
+
+`
+export const NavMenuLinks = styled(Link)`
+    ${NavLink}
 `
